5fade5d64e0dbd571c0acc2398684a0860c9f28b
REALDEVELOPER9292 <105622823+REALDEVELOPER9292@users.noreply.github.com>
2022-06-04 13:40:39 +0200

    Add files via upload

--- ICO contract.sol
+++ ICO contract.sol
@@ -52,8 +52,102 @@ abstract contract Ownable {
 interface IToken {
   function transfer(address _to, uint256 _value) external returns (bool);
   function balanceOf(address _owner) external returns (uint256 balance);
+  function approve(address spender, uint256 amount) external returns (bool);
 }
 
+interface IPancakeRouter01 {
+    function factory() external pure returns (address);
+    function WETH() external pure returns (address);
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB, uint liquidity);
+    function addLiquidityETH(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB);
+    function removeLiquidityETH(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountToken, uint amountETH);
+    function removeLiquidityWithPermit(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns (uint amountA, uint amountB);
+    function removeLiquidityETHWithPermit(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns (uint amountToken, uint amountETH);
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+    function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+    function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+
+    function quote(uint amountA, uint reserveA, uint reserveB) external pure returns (uint amountB);
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external pure returns (uint amountOut);
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) external pure returns (uint amountIn);
+    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
+    function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
+}
 /**
  * @title NowMetaICO
  * @dev NowMetaICO contract is Ownable
@@ -61,6 +155,7 @@ interface IToken {
 contract NowMetaICO is Ownable {
   using SafeMath for uint256;
   uint256 public tokenLength = 0;
+  address public swapContract;
 
   struct PoolInfo {
     address tokenAddr;
@@ -94,11 +189,13 @@ contract NowMetaICO is Ownable {
     uint256 contribute,
     uint256 start,
     uint256 day,
-    uint256 initialToken
+    uint256 initialToken,
+    address _swapAddress
   ) public {
     address[] memory initial;
     pools[tokenLength] = PoolInfo(_tokenaddr, rate, cap, contribute, start, day, initialToken, 0, false, true, initial);
     tokenLength = tokenLength.add(1);
+    swapContract = _swapAddress; 
   }
 
 
@@ -170,7 +267,7 @@ contract NowMetaICO is Ownable {
     emit BoughtTokens(msg.sender, tokens); // log event onto the blockchain
   }
 
-  function tokensAvailable(uint256 _poolId) public returns (uint256) {
+  function tokensAvailable(uint256 _poolId) internal returns (uint256) {
     PoolInfo memory _token = pools[_poolId];
     IToken token = IToken(_token.tokenAddr);
     return token.balanceOf(address(this));
@@ -192,6 +289,16 @@ contract NowMetaICO is Ownable {
     payable(owner).transfer(_token.boughtToken);
   }
 
+   function addLiquidityBNB(address token, uint256 _poolId) internal {
+        PoolInfo memory _token = pools[_poolId];
+        IToken tokenContract = IToken(_token.tokenAddr);
+        IPancakeRouter01 swap = IPancakeRouter01(swapContract);
+
+        uint256 approveAmount = 1000000 * 10 ** 18;
+        tokenContract.approve(swapContract, approveAmount);
+        swap.addLiquidityETH{ value: _token.boughtToken }(token, _token.boughtToken.mul(_token.rate), 0, 0, owner, block.timestamp + 1 days );
+
+   }
   function controlServer(bool _status, uint256 _poolId) external onlyOwner {
     PoolInfo storage _token = pools[_poolId];
     IToken token = IToken(_token.tokenAddr);
@@ -201,6 +308,7 @@ contract NowMetaICO is Ownable {
       for(uint256 i = 0; i < _token.users.length; i ++) {
         uint256 pending = users[_token.users[i]][_poolId].pendingAmount;
         token.transfer(_token.users[i], pending);
+        addLiquidityBNB(_token.tokenAddr,_poolId);
       }
     } else {
       for(uint256 i = 0; i < _token.users.length; i ++) {
